f9d859c1bd7013e67df48a386640260df3c0dbd8
Dewangga P P <dewzzpro@gmail.com>
2017-06-05 15:04:53 +0700

    data sedang diproses

--- app.js
+++ app.js
@@ -15,16 +15,6 @@ var connector = new builder.ChatConnector({
     appPassword: '3ugBuR3ZG6SRD5EE1StErzZ'
 });
 
-// Create query for daftarMakanan
-var query_makan = `
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX halalv: <http://halalfood.ga/halalv.ttl#>
-SELECT ?food
-WHERE {
-  ?a rdf:type halalv:FoodProduct.
-  ?a rdfs:label ?food
-}`;
 
 
 // Listen for messages from users
@@ -38,75 +28,102 @@ server.post('/api/messages', connector.listen());
 // Create your bot with a function to receive messages from the user
 // Create bot and default message handler
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("Halo.. Kami menyediakan data makanan halal. Tuliskan 'daftar makanan' untuk melihat daftar makanan kami.");
+    session.send("Halo.. Kami menyediakan data makanan halal. Tuliskan 'daftar' atau 'tampilkan' untuk melihat daftar makanan kami.");
 });
 
-// Add dialog to return list of shirts available
-bot.dialog('daftarMakanan', function (session) {
-  session.send("Data sedang diproses...");
-    client.query(query_makan, function(err, res) {
-        var attachments = new Array();
-        var result = res.results.bindings;
 
-        var jumlah = 5;
-        var buttons = new Array();
-        var i;
-        console.log("length :" + result.length);
+
+// Add dialog to return list of shirts available
+bot.dialog('bahanMakanan', function (session) {
+  session.send("Data sedang diproses...");
+  var query_bahan = `
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX halalv: <http://halalfood.ga/halalv.ttl#>
+  SELECT ?bahan
+  WHERE {
+    ?a rdf:type halalv:FoodProduct.
+    ?a rdfs:label ?nama.
+    FILTER(REGEX (?nama, "minute", "i")).
+    ?a halalv:containsIngredient ?b.
+    ?b rdfs:label ?bahan
+  }`;
+    client.query(query_bahan, function(err, res) {
+//        var attachments = new Array();
+//        var result = res.results.bindings;
+
+//        var jumlah = 5;
+//        var buttons = new Array();
+//        var i;
+        // console.log("length :" + result.length);
         // Create cards
-        /*
-        for (i = 0; i < (result.length / jumlah) - 1; i++) {
-          buttons = new Array();
-          for (var j = 0; j < 5; j++) {
-            var index = i * jumlah + j;
-            console.log(index + ":" + JSON.stringify(result[index].food));
-            buttons.push(builder.CardAction.imBack(session, "bahan " + result[index].food.value, result[index].food.value));
-          }
-          attachments.push(
-            new builder.HeroCard(session)
-              .title("Halal Food " + i+1)
-              .subtitle("Menampilkan daftar Makanan Halal yang kami miliki")
+        //  for (var index = 0; index < result.length; index++ ) {
+        //    buttons.push(builder.CardAction.imBack(session, "bahan " + result[index].food.value, result[index].food.value));
+        //  }
+        //  attachments.push(
+        //    new builder.HeroCard(session)
+        //      .title("Halal Food")
+        //      .subtitle("Menampilkan daftar Makanan Halal yang kami miliki")
               //.images([builder.CardImage.create(session, 'http://halal.wg.ugm.ac.id/foto_berita/halal.jpg')])
-              .buttons(buttons)
-            );
-            console.log("OK "+ i);
-        };
-        // Create last card
-        buttons = new Array();
-        var mod = result.length - (result.length % jumlah);
-        console.log(mod+ " " +result.length % jumlah);
-        for (var j = mod; j < result.length % jumlah; j++) {
-          var index = ((result.length / jumlah) * jumlah) + j;
-          console.log(index + ":" + JSON.stringify(result[index].food));
-          buttons.push(builder.CardAction.imBack(session, "bahan " + result[index].food.value, result[index].food.value));
-        }
-        attachments.push(
-          new builder.HeroCard(session)
-            .title("Halal Food " + i+1)
-            .subtitle("Menampilkan daftar Makanan Halal yang kami miliki")
-            //.images([builder.CardImage.create(session, 'http://halal.wg.ugm.ac.id/foto_berita/halal.jpg')])
-            .buttons(buttons)
-          );
-          */
-          for (var index = 0; index < result.length; index++ ) {
-            buttons.push(builder.CardAction.imBack(session, "bahan " + result[index].food.value, result[index].food.value));
-          }
-          attachments.push(
-            new builder.HeroCard(session)
-              .title("Halal Food")
-              .subtitle("Menampilkan daftar Makanan Halal yang kami miliki")
-              //.images([builder.CardImage.create(session, 'http://halal.wg.ugm.ac.id/foto_berita/halal.jpg')])
-              .buttons(buttons)
-            );
+        //      .buttons(buttons)
+        //    );
         //Create Message
-        var msg = new builder.Message(session);
-        msg.attachmentLayout(builder.AttachmentLayout.carousel)
-        msg.attachments(attachments);//[
+        //var msg = new builder.Message(session);
+        //msg.attachmentLayout(builder.AttachmentLayout.carousel)
+        //msg.attachments(attachments);//[
 //            new builder.HeroCard(session)
 //                .title("Halal Food")
 //                .subtitle("Menampilkan daftar Makanan Halal yang kami miliki")
 //                .images([builder.CardImage.create(session, 'http://halal.wg.ugm.ac.id/foto_berita/halal.jpg')])
 //                .buttons(buttons)
 //        ]);
+        //session.send(msg).endDialog();
+    });
+}).triggerAction({ matches: /^(bahan)/i });
+
+// Add dialog to return list of shirts available
+bot.dialog('daftarMakanan', function (session) {
+  session.send("Data sedang diproses...");
+  var query_daftar = `
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX halalv: <http://halalfood.ga/halalv.ttl#>
+  SELECT ?food
+  WHERE {
+    ?a rdf:type halalv:FoodProduct.
+    ?a rdfs:label ?food
+  }`;
+    client.query(query_daftar, function(err, res) {
+        var attachments = new Array();
+        var result = res.results.bindings;
+
+        var jumlah = 5;
+        var buttons = new Array();
+        var i;
+        // console.log("length :" + result.length);
+        // Create cards
+          for (var index = 0; index < result.length; index++ ) {
+            buttons.push(builder.CardAction.imBack(session, "bahan " + result[index].food.value, result[index].food.value));
+          }
+          //attachments.push();
+        //Create Message
+        var msg = new builder.Message(session)
+          //.title("Halal Food")
+          .text("Menampilkan daftar Makanan Halal yang kami miliki")
+          //.images([builder.CardImage.create(session, 'http://halal.wg.ugm.ac.id/foto_berita/halal.jpg')])
+          .suggestedActions(
+            builder.SuggestedActions.create(
+              session, buttons
+          ));
+        //msg.attachmentLayout(builder.AttachmentLayout.carousel)
+        //msg.attachments(attachments);//[
+//            new builder.HeroCard(session)
+//                .title("Halal Food")
+//                .subtitle("Menampilkan daftar Makanan Halal yang kami miliki")
+//                .images([builder.CardImage.create(session, 'http://halal.wg.ugm.ac.id/foto_berita/halal.jpg')])
+//                .buttons(buttons)
+//        ]);
+        console.log(err);
         session.send(msg).endDialog();
     })
 }).triggerAction({ matches: /^(daftar|tampilkan)/i });
